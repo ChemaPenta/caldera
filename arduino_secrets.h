ce209681ce61ed6eac29baf6d4f9a63e80ed2a73
Chema <chema@pentabyte.es>
2020-01-29 21:00:09 +0100

    secrets empty
    El archivo de secretos vacío

---
+++ arduino_secrets.h
@@ -0,0 +1,7 @@
+#define SECRET_SSID ""
+#define SECRET_PASS ""
+
+#define MQTT_SERVER      ""
+#define MQTT_SERVERPORT  1883                   // use 8883 for SSL
+#define MQTT_USERNAME    ""
+#define MQTT_PW          ""
