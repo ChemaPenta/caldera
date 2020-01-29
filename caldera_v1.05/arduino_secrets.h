5445b7d58f8c9026d3ea58c2ff692b94c37cf090
Chema <chema@pentabyte.es>
2020-01-29 21:03:55 +0100

    Delete arduino_secrets.h

--- caldera_v1.05/arduino_secrets.h
+++
@@ -1,7 +0,0 @@
-#define SECRET_SSID ""
-#define SECRET_PASS ""
-
-#define MQTT_SERVER      ""
-#define MQTT_SERVERPORT  1883                   // use 8883 for SSL
-#define MQTT_USERNAME    ""
-#define MQTT_PW          ""
